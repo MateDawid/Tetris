697eba47036124b640262daee0cf57d8bac4e7c4
Dawid <mateusiakdawid@gmail.com>
2021-04-18 14:15:45 +0200

    Add piece moving and rotating

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ class Tetris():
         self.width = 22
         self.height = 22
         self.board = self.create_board()
+        self.piece =
+        self.piece_position = [0, 10]
+
 
     def create_board(self):
         # row = [1] + [0 for _ in range(self.width)] + [1]
@@ -42,20 +45,35 @@ class Tetris():
              [1, 1]]
         ]
 
-        return pieces[random.randrange(len(pieces))]
+        self.piece = pieces[random.randrange(len(pieces))]
 
     def add_new_piece(self):
-        piece = self.select_piece()
-        y = len(piece)
-        x = len(piece[0])
-        if x == 4:
-            for i in range(y):
-                for j in range(x):
-                    self.board[i][9+j] = piece[i][j]
-        else:
-            for i in range(y):
-                for j in range(x):
-                    self.board[i][10+j] = piece[i][j]
+        self.piece_position = [0, 10]
+        y = len(self.piece)
+        x = len(self.piece[0])
+        for i in range(y):
+            for j in range(x):
+                self.board[self.piece_position[0]+i][self.piece_position[1]+j] = self.piece[i][j]
+
+
+    def move_left(self):
+        self.piece_position = [self.piece_position[0]+1, self.piece_position[1] - 1]
+
+    def move_right(self):
+        self.piece_position = [self.piece_position[0]+1, self.piece_position[1] + 1]
+
+    def rotate_clocwise(self):
+        self.piece = list(list(elem) for elem in zip(*(self.piece[::-1])))
+        self.piece_position = [self.piece_position[0] + 1, self.piece_position[1]]
+
+    def rotate_anticlockwise(self):
+        first_rotation = list(list(elem) for elem in zip(*(self.piece[::-1])))
+        second_rotation = list(list(elem) for elem in zip(*first_rotation))
+        third_rotation = list(list(elem) for elem in zip(*second_rotation))
+        self.piece = third_rotation
+        self.piece_position = [self.piece_position[0] + 1, self.piece_position[1]]
+
+
 
 
 if __name__ == "__main__":
